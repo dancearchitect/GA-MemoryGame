5631eac89489ab97015d57c9acf8bf13bdadd981
Rachel Gonzalez <rachelgonzalez@Rachels-MBP.home>
2019-03-10 17:57:49 -0400

    created game logic

--- memory_game/html/main.js
+++ memory_game/html/main.js
@@ -1,17 +1,19 @@
 
-var card = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = card[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped Queen");
-var carTwo = card[4];
-cardsInPlay.push('cardTwo');
-console.log("User flipped King");
-
-if (cardsInPlay.length === 2)
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2)
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
 	}
 	else {
 		alert("Sorry, try again!")
 	}
+}
+var flipCard = function(cardsID) {
+	console.log("User flipped" + " " + cardsID)
+cardsInPlay.push(cards[cardsID]);
+checkForMatch()
+}
+flipCard();
+flipCard(2);
