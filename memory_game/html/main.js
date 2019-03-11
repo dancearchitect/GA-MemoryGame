b6f47f074465573c6d4a364e5004b9dd5bdd61fd
Rachel Gonzalez <rachelgonzalez@Rachels-MBP.home>
2019-03-10 20:58:00 -0400

    created game logic

--- memory_game/html/main.js
+++ memory_game/html/main.js
@@ -1,19 +1,72 @@
+//all cards
+var cards = [
+ {
+   rank: "queen",
+   suit: "hearts",
+   cardImage: "images/queen-of-hearts.png"
+ },
+ {
+   rank: "queen",
+   suit: "diamonds",
+   cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+   rank: "king",
+   suit: "hearts",
+   cardImage: "images/king-of-hearts.png"
+ },
+ {
+   rank: "king",
+   suit: "diamonds",
+   cardImage: "images/king-of-diamonds.png"
+ }
+];
 
-var cards = ["queen", "queen", "king", "king"];
+//array with cards in play
 var cardsInPlay = [];
+
+//function that checks card matches
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2)
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} 
+		else {
+			alert("Sorry, try again.");
+		}
 	}
-	else {
-		alert("Sorry, try again!")
+};
+
+//function that executes card flips
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+//push the cards to cardID array
+ cardsInPlay.push(cards[cardId].rank)
+//test code
+ console.log("User flipped " + cards[cardId].rank)
+ console.log(cards[cardId].cardImage)
+ console.log(cards[cardId].suit)
+//call the function to check for a match
+ checkForMatch ();
+};
+
+//function new game board
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
+};
+
+function pageReset() {
+  document.getElementById('reset-button');
 }
-var flipCard = function(cardsID) {
-	console.log("User flipped" + " " + cardsID)
-cardsInPlay.push(cards[cardsID]);
-checkForMatch()
-}
-flipCard();
-flipCard(2);
+
+//call the function game board
+createBoard();
+//reset game
+pageReset();
